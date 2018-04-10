904b84255736f7eb1e5815097dde80187e70b647
Ty Thompson <tylermoko@gmail.com>
2018-04-10 10:55:37 -0400

    adding axios and testing api calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends Component {
+
   
   render() {
     return (
@@ -11,6 +14,16 @@ class App extends Component {
     );
   }
 }
+
+// weather?zip=94040,us
+
+axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=08083,us&APPID=${API_KEY}`)
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
 
 console.log(API_KEY);
 export default App;
