301458e24a5f181ac80e1df7d83be69e43874b5b
Ty Thompson <tylermoko@gmail.com>
2018-04-10 10:25:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DiscGolf Weather App
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+console.log(API_KEY);
+export default App;
